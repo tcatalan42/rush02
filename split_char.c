ebdcebcd77739b37cf838a4911cbe309cf7a47da
Thomas Catalano <tcatalan@fu-r10-p2.s19.be>
2026-03-22 04:53:19 +0100

    norminette ok!

--- split_char.c
+++ split_char.c
@@ -6,81 +6,80 @@
 /*   By: tcatalan <tcatalan@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 22:41:55 by tcatalan          #+#    #+#             */
-/*   Updated: 2026/03/22 01:47:23 by tcatalan         ###   ########.be       */
+/*   Updated: 2026/03/22 04:52:59 by tcatalan         ###   ########.be       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdio.h>	// ------ A SUPPRIMER
 #include <stdlib.h>
 
-int		ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int		i;
 
 	i = -1;
-	while(str[++i]);
+	while (str[++i])
+		;
 	return (i);
+}
+
+char	*extract_block(char *str, int start_index, int size)
+{
+	char	*block;
+	int		i;
+
+	i = -1;
+	block = malloc((size + 1) * sizeof(char));
+	while (++i < size)
+	{
+		block[i] = str[start_index];
+		start_index++;
+	}
+	block[i] = '\0';
+	return (block);
 }
 
 char	**split_char(char *str)
 {
 	char	**res;
 	int		len;
-	int		first_block_size;
-	int		i;
-	char	*first_block;
-	char	*block;
 	int		j;
+	int		first_block_size;
 	int		block_index;
 
 	len = ft_strlen(str);
-
 	if (len % 3 == 0)
 		res = malloc(((len / 3) + 1) * sizeof(char *));
 	else
 		res = malloc(((len / 3) + 2) * sizeof(char *));
-
 	if (len % 3 == 0)
 		first_block_size = 3;
 	else
 		first_block_size = len % 3;
-
-	first_block = malloc((first_block_size + 1) * sizeof(char));
-	i = -1;
-	while(++i < first_block_size)
-		first_block[i] = str[i];
-	first_block[i] = '\0';
 	block_index = 0;
-	res[block_index] = first_block;
-	i = -1;
+	res[block_index] = extract_block(str, 0, first_block_size);
 	j = first_block_size - 1;
 	while (++j < len)
 	{
-		block = malloc((3 + 1) * sizeof(char));
-		while (++i < 3)
-		{
-			block[i] = str[j];
-			j++;
-		}
-		block[i] = '\0';
-		res[++block_index] = block;
-		i = -1;
-		j -= 1;
+		res[++block_index] = extract_block(str, j, 3);
+		j += 2;
 	}
 	res[++block_index] = NULL;
 	return (res);
 }
 
-int		main(void)
+int	main(void)
 {
-	char **result;
+	char	**result;
 	int		i;
 
-	result = split_char("1234666436347843678784389548735489345768348978348943783489347823490344647457376373536567");
+	result = split_char("123466978348943783489347823490344647457376373536567");
 	i = -1;
-
 	while (result[++i])
 		printf("bloc %d = %s\n", i, result[i]);
-	
+	i = -1;
+	while (result[++i])
+		free(result[i]);
+	free(result);
 	return (0);
 }
